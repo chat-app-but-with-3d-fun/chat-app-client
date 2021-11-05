c7e2a0d2a77a25224ea8b2d077c9bdc5e301aff6
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-11-05 18:03:23 +0100

    adding provider

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import ParamChild from './features/components/paramChild';
-import { useUpdate } from 'react-three-fiber'
-
-const Routing = () => {
-  return(
-    <Router>
-      <Switch>
-        <Route path="/" exact component={App} />
-        {/* <Route path="/:id" component={ParamChild} /> */}
-      </Switch>
-    </Router>
-  )
-}
-
+import { Provider } from 'react-redux';
+import store from './features/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routing />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-
