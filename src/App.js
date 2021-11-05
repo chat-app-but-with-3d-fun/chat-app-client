483482690fcfb069261c132d29ae3e1bc986da9e
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-11-05 19:24:23 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-
 import React from "react";
-import { Switch, Route, Router } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from './pages/Dashboard';
 import Register from "./pages/Register";
 import "./sass/main.scss";
 
-
-function App() {
-  //const { user } = useContext(UserContext);
-  // console.log('this is user',user)
+const App = () => {
 
   return (
-
-      <div className="App">
-      <Switch>       
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signup" component={Register} />
-        {/* <Route exact path="/login" component={Dashboard} /> */}
-      </Switch>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/sign-:form" component={Register} />
+          {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+        </Switch>
+      </Router>
     </div>
-
-
   );
 }
 export default App;
