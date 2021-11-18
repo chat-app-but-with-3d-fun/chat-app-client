c03f69893314d5127193b7b6e93579f11dedefc4
Aimee Harkin <aimeeharkin@Aimees-MBP.fritz.box>
2021-11-18 10:07:08 +0100

    up to do date 18.11

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,67 +1,85 @@
 
-import React, { Suspense, useRef, useState } from "react";
-import { Canvas,  useLoader, useFrame } from 'react-three-fiber';
-import { OrbitControls, Stars, Html, Text, useTexture } from '@react-three/drei';
+import React, { Suspense, useRef, useState, useEffect } from "react";
+import { Canvas,  useLoader, useFrame, primitive } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { Environment, OrbitControls, Stars, useAnimations, useGLTF } from '@react-three/drei';
+// import { usePlane } from "@react-three/cannon"
+import { useSpring, animated } from '@react-spring/three'
 
 function Box(props){
   return (
     <mesh {...props}>
-      <boxGeometry attach='geometry' position={[-1, 1, 0]}/>
+      <boxGeometry attach='geometry' position={[-1, 1, 0]} />
       <meshLambertMaterial attach='material' color='hotpink' />
+    </mesh>
+  )
+}
 
-      {/* <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial attach='material' color='lightblue' />       */}
+function Plane(props){
+  return(
+
+    <mesh>
+      <planeGeometry attach="geometry" {...props} />
+      <meshStandardMaterial attach="material" color="#fff" />
     </mesh>
   )
 }
 
 function Sphere(props){
-  // const colorMap = useLoader(TextureLoader, './textures/highpoly_baseColor.jpg')
-// const gltf = useLoader(GLTFLoader, './pegasus/scene.gltf');
+
   return(
     <>
     <ambientLight intensity={0.2} />
     <directionalLight />
     <mesh>
-      <sphereGeometry args={[1, 32, 32]}  />
+      <sphereGeometry args={[1, 32, 32]}  {...props} />
       <meshStandardMaterial color='red' />
     </mesh>
     </>
-
-    // <Suspense fallback={null}>
-    // <primitive object={gltf.scene} {...props}/>
-    // </Suspense>
-
   )
 }
 
 function Pegasus(props){
-  const gltf = useLoader(GLTFLoader, './pegasus/scene.gltf');
+  const url = './pegasus/scene.gltf';
+  const gltf = useLoader(GLTFLoader, url);
+  const { nodes, materials, animations } = useGLTF(url)
+  const { ref, mixer, names, actions, clips } = useAnimations(animations, gltf.scene)
 
+  console.log(actions);
+  useEffect(() => {
+  actions["metarig|HorseWalk"].play()
+  })
+
+  console.log(gltf);
   return(
-    <Suspense fallback={null}>
-    <primitive object={gltf.scene} {...props}/>
+    <Suspense fallback={null} >
+    <primitive object={gltf.scene} {...props} 
+    // onClick={() =>
+    //   // This makes the object fly upwards when clicked on
+    //   gltf.applyImpulse([0, 5, -10], [1, 1, 1])}
+      />
     </Suspense>
   )
 }
 
 function Pear(props){
   const pear = useLoader(GLTFLoader, './pear/scene.gltf')
-
+  console.log(pear);
   return(
     <Suspense fallback={null}>
-      <primitive object={pear.scene} {...props} />
+      <primitive object={pear.scene} scale={0.5} {...props} />
     </Suspense>
   )
+
 }
+
+
 
 function Bar(props){
   const bar = useLoader(GLTFLoader, './classicbar/scene.gltf')
   return(
-    <Suspense fallback={null}>
-      <primitive object={bar.scene} scale={2} {...props} />
+    <Suspense fallback={null} castShadow receiveShadow>
+      <primitive object={bar.scene} scale={8} {...props} />
     </Suspense>
   )
 }
@@ -69,8 +87,8 @@ function Bar(props){
 function Robot(props){
   const robot = useLoader(GLTFLoader, './robot/scene.gltf')
   return(
-    <Suspense fallback={null}>
-      <primitive object={robot.scene}{...props}/>
+    <Suspense fallback={null} args={[1, 1, 0]}>
+      <primitive object={robot.scene} scale={3} {...props}/>
     </Suspense>
   )
 }
@@ -79,7 +97,7 @@ function Streetlight(props){
   const streetlight = useLoader(GLTFLoader, './streetlight/scene.gltf')
   return(
     <Suspense fallback={null}>
-      <primitive object={streetlight.scene} scale={0.5}{...props}/>
+      <primitive object={streetlight.scene} scale={0.5} {...props}/>
     </Suspense>
   )
 }
@@ -90,59 +108,26 @@ export default function Home(props){
     <div>
       <h2>welcome to koko!</h2>
     </div>
-    <Canvas style={{width: '100%' , height: '100%' }} camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 5] }} {...props}>      
+    <Canvas 
+    style={{width: '100%' , height: '100%' }} camera={{ fov: 75, near: 0.1, far: 1000, position: [3, 3, 5 ] }} 
+    // camera={{ position: [0, 0, 0], near: 0.1, far: 1000 }}
+    {...props}>      
     <color attach='background' args={'black'}/>
       <Suspense fallback={null}>
         <OrbitControls />
         <Stars fade />
         <ambientLight intensity={1} />
         <spotLight position={[10, 15, 10]} angle={0.3} /> 
-        <Box position={[2.5, 2.5, 2.5]}/>
-        <Sphere position={[2.5,2.5,5]} />
-        <Pegasus position={[1,1,1]} />
-        <Pear position={[3, 3, 3]} />
-        <Bar position={[-1,-1,0]} invalidate/>
-        <Robot position={6,6,6}/>
-        <Streetlight position={7,7,7}/>
+        <Plane position={[0,0,0]} />
+        {/* <Box position={[2.5, 2.5, 2.5]}/>
+        <Sphere position={[4,4,4]} /> */}
+        <Pegasus position={[0,0,0]} />
+        <Pear position={[0,0,0]} />
+        {/* <Bar position={[0,0,0]} invalidate/> */}
+        <Robot position={0,0,0}/>
+        <Streetlight position={0,0,0} scale={0.5}/>
       </Suspense>
     </Canvas>
     </>
   )
 }
-
-
-// function BoxOne(props) {
-
-//   return (
-//     <mesh {...props}>
-//       <boxGeometry attach='geometry' position={[-1, 1, 0]}/>
-//       <meshLambertMaterial attach='material' color='hotpink' />
-//     </mesh>
-//   )
-// }
-
-// export default function Home(){
-//   return(
-//     <Canvas>
-//       <Suspense fallback={null}>
-//         <ambientLight />
-//       <pointLight position={[10, 10, 10]} />
-//       <BoxOne position={[-1.2, 0, 0]} />
-//       <Box position={[3, 0, 0]} />
-//       <Sphere position={[0,0,0]} />
-//       <Pegasus position={[2,2,2]} />
-//       </Suspense>
-    
-//   </Canvas>
-//   )
-// }
-
-// ReactDOM.render(
-  // <Canvas>
-  //   <ambientLight />
-  //   <pointLight position={[10, 10, 10]} />
-  //   <Box position={[-1.2, 0, 0]} />
-  //   <Box position={[1.2, 0, 0]} />
-  // </Canvas>,
-//   document.getElementById('root')
-// )
