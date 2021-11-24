7affa23f1bc8ba91aa6f34881e8ad6a6753571fe
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-11-24 16:37:09 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Home from "./pages/Home";
 import Dashboard from './pages/Dashboard';
 import Register from "./pages/Register";
@@ -8,9 +8,10 @@ import "./sass/main.scss";
 import Sidebar from "./pages/Sidebar";
 import Room from "./pages/Room";
 import { socket, useAuthUserMutation } from "./features/api/apiSlice";
-import { setUser } from "./features/user/userSlice";
+import { selectUserId, setUser, updateFriendStatus } from "./features/user/userSlice";
 
 const App = () => {
+  const userId = useSelector(selectUserId)
   const [ authUser, { data, isSuccess } ] = useAuthUserMutation()
 
   const dispatch = useDispatch()
@@ -25,6 +26,16 @@ const App = () => {
       }
     }
     auth()
+
+    if (socket) {
+      socket.on('unRegister', (friendId) => {
+        console.log('friendID => ', friendId)
+        socket.emit('handshake', friendId)
+        dispatch(
+          updateFriendStatus(friendId)
+        )
+      })
+    }
   }, [])
 
   return (
