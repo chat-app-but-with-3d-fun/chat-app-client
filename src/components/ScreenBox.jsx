0fa0c4035df90e30d45470d74fb61e29ab092c01
puckfried <puckfried@posteo.de>
2021-11-24 16:21:11 +0100

    screenBox

--- src/components/ScreenBox.jsx
+++ src/components/ScreenBox.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import { Container, Button, ButtonGroup, Box, Paper, Grid, Divider, TextField, Typography, List, ListItem, ListItemIcon, ListItemText, Avatar, Fab } from '@mui/material'
+import JitsiRemoteVideo from './JitsiRemoteVideo'
 
-export default function ScreenBox() {
+
+export default function ScreenBox(props) {
+    const {screenShare, setScreenShare} = props
     return (
-        <div>
-            
-        </div>
+        <Paper
+        elevation="10"
+        sx={{display: 'flex',
+            flexDirection: 'column'}}
+        >
+            <Grid item xs={12}>
+                <Typography variant='h5' align='center'>Screen Share</Typography>
+                <Button onClick={() => setScreenShare(() => !screenShare)}>Show Screen</Button>
+            </Grid>
+
+        </Paper>
     )
 }
