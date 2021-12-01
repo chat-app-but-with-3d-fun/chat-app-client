3df0686f957d5d8d0881de0080e2651de81f0dea
Vasileios Psychas <vaspsych@gmail.com>
2021-12-01 11:09:54 +0100

    Setting up model init in room

--- src/components/Chat3D.js
+++ src/components/Chat3D.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, Suspense } from 'react';
 import { useSelector } from 'react-redux';
-import {selectActiveUsers, selectRoom} from '../features/room/roomSlice'
+import { selectActiveUsers, selectRoom } from '../features/room/roomSlice';
 
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
@@ -10,38 +10,41 @@ import { default as Robot } from '../models/Robot.jsx';
 import { default as Astro } from '../models/Astro.jsx';
 
 const Chat3D = ({ location }) => {
+  const [models, setModels] = useState();
+  const activeUsers = useSelector(selectActiveUsers);
+  const room = useSelector(selectRoom);
+
+  const ownUser = [
+    <Robot key={'ownAvatar'} position={[0, 0, 40]} rotation={[0, 180, 0]} />,
+  ];
 
   useEffect(() => {
     //  We check how many  users are there and we inititiate them and add them in the scenes
-    let findModels = selectActiveUsers.map((mod, index) => {
+    console.log('This room has active users', activeUsers)
+    let findModels = activeUsers.map((mod, index) => {
       if (index === 0) {
-        console.log('CHAT3D LOADS THE ROBOT');
-        return <Robot key={index} position={[0, 0, 40]} rotation={[0, 180, 0]} />;
-      }
-      if (index === 1) {
         console.log('CHAT3D LOADS THE PEGASUS');
         return <Pegasus key={index} position={[0, 0, -40]} />;
       }
-      if (index === 2) {
+      if (index === 1) {
         console.log('CHAT3D LOADS THE BASEBALL');
         return <Baseball key={index} position={[40, 0, 0]} />;
       }
 
-      if (index === 3) {
+      if (index === 2) {
         console.log('CHAT3D LOADS THE ASTRO');
-        return <Astro key={index} position={[-40, 0, 0]} rotation={[0, 90, 0]}/>;
+        return <Astro key={index} position={[-40, 0, 0]} rotation={[0, 90, 0]} />;
       }
     });
 
     setModels(findModels);
-  }, []);
+  }, [activeUsers]);
 
   console.log('********** 3D  CHAT*************');
   console.log('Location', location);
   console.log('modelsToLoad', models);
   console.log('****************************');
 
-  
   return (
     <div className='chat3d' style={{ width: '100%', height: '100%' }}>
       <Canvas
@@ -53,7 +56,10 @@ const Chat3D = ({ location }) => {
         <Stars fade />
         <ambientLight intensity={1} />
         <spotLight position={[0, 0, 0]} />
-        <Suspense fallback={null}>{ownUser[0]}{models}</Suspense>
+        <Suspense fallback={null}>
+          {ownUser[0]}
+          {models}
+        </Suspense>
       </Canvas>
     </div>
   );
