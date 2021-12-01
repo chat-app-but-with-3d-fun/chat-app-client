1bb6a506a04ed101bcc995bd808d97bc475487af
puckfried <puckfried@posteo.de>
2021-12-01 15:18:37 +0100

    concurrent

--- src/components/Chat3D.js
+++ src/components/Chat3D.js
@@ -46,17 +46,18 @@ const Chat3D = ({ location }) => {
   
   return (
     <div className='chat3d' style={{ width: '100%', height: '100%' }}>
-      {/* <Canvas
+      <Canvas
         style={{ width: '100%', height: '100%' }}
         camera={{ fov: 75, near: 0.1, far: 1000, position: [-20, 30, 50] }}
-      > */}
+        mode='concurrent'
+      >
         {/* <color attach='background' args={['black']} /> */}
         {/* <OrbitControls />
         <Stars fade />
         <ambientLight intensity={1} />
         <spotLight position={[0, 0, 0]} /> */}
         {/* <Suspense fallback={null}>{ownUser[0]}</Suspense>  */}
-      {/* </Canvas> */}
+      </Canvas>
     </div>
   );
 };
