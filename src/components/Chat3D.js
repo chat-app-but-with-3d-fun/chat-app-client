4db6f866afe1bcf543264dc539bd68c3d337fc54
puckfried <puckfried@posteo.de>
2021-12-02 09:57:03 +0100

    without canvas

--- src/components/Chat3D.js
+++ src/components/Chat3D.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
+// import { Canvas } from '@react-three/fiber';
 
 
 
@@ -8,8 +8,8 @@ const Chat3D = ({ location }) => {
   
   return (
     <div className='chat3d' style={{ width: '100%', height: '100%' }}>
-      <Canvas>
-      </Canvas>
+      {/* <Canvas>
+      </Canvas> */}
     </div>
   );
 };
