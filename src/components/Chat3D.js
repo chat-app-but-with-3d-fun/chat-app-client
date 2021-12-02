0da75811e33659a012f2e1f42409d39a4a3dfad6
puckfried <puckfried@posteo.de>
2021-12-02 10:48:44 +0100

    uncommented canvas

--- src/components/Chat3D.js
+++ src/components/Chat3D.js
@@ -8,8 +8,8 @@ const Chat3D = ({ location }) => {
   
   return (
     <div className='chat3d' style={{ width: '100%', height: '100%' }}>
-      {/* <Canvas>
-      </Canvas> */}
+      <Canvas>
+      </Canvas>
     </div>
   );
 };
